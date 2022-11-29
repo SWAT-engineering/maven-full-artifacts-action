1343a81b2c11754ead763890ab9207cd9f5a608b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2022-11-29 10:54:36 +0100

    use core.getMultiLineInput to get newline split and trimmed results from the actions file

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function run(): Promise<void> {
       [
         '-B',
         '-X',
-        ...(core.getInput('maven-options').split(' ').trim()),
+        ...core.getMultilineInput('maven-options'),
         '-Dmaven.test.skip=true',
         '-DskipTests',
         `-DaltDeploymentRepository=${localMavenRepo}`,
