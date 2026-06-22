632034507d96835d3eaa49318ff11d1dcab9ea6d
Davy Landman <davy.landman@swat.engineering>
2026-06-22 10:57:30 +0200

    Different require

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import { createRequire } from 'module'; const require = createRequire(import.meta.url);
+globalThis.require ??= (await import('node:module')).createRequire(import.meta.url);
 var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
